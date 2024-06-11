c583e8967cea2dc0899bc74b04db810304afd42d
cyberpropper <osankin2002@mail.ru>
2024-06-12 01:27:34 +0700

    gotovo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,60 @@
+import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void printLeapYearStatus(int year) {
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - не високосный год");
+        }
     }
+
+
+    public static void softwareVersion(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        byte clientOS = 1;
+        if (deviceYear < currentYear) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
+
+    public static int deliveryDistance(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2024;
+        printLeapYearStatus(year);
+
+        int deviceYear = 1999;
+        softwareVersion(deviceYear);
+
+        int deliveryDistance = 90;
+        int days = deliveryDistance(deliveryDistance);
+        if (days > 0) {
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставки нет.");
+        }
+    }
+
 }
